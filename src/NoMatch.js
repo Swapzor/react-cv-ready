d7a74456e8b1ee0f359a8669e70665b8865191ca
Swapzor <marcinkacki94@gmail.com>
2020-05-28 20:20:24 +0200

    nomatch

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NoMatch = () => (
         <div>
-            <h2>NoMatch</h2>
-            <p>asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf</p>
+            <h1>404</h1>
+            <h3>Page not found!</h3>
         </div>
 )
     
